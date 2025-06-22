3185b8e020c83944493ee4c1c57c7e42311cb446
nicolasoarantes <nicolasoarantes@gmail.com>
2025-06-22 13:42:31 -0300

    quarto3_D

---
+++ models/mini_quarto_3_d_rosa/app/page.tsx
@@ -0,0 +1,11 @@
+import Spline from '@splinetool/react-spline/next';
+
+export default function Home() {
+  return (
+    <main>
+      <Spline
+        scene="https://prod.spline.design/IngjYToW2o48jZ6F/scene.splinecode" 
+      />
+    </main>
+  );
+}
